325c2d8c8de9a4fb326609909b3a3fff61faf7f2
hamza aboulmaali 'Calim <rica.hamza@yahoo.fr>
2024-07-17 08:47:20 +0100

    last update Navbra

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
                     <img src={logo} alt="Logo" className="" />
 
                     <div onClick={handelClick} className='fixed items-end right-0 pr-12 pt-[13px] md:hidden lg:hidden'>
-                        <FaBarsStaggered className="w-6 h-6 text-white cursor-pointer " />
+                        <FaBarsStaggered className="w-6 h-6 text-red-500 cursor-pointer " />
                     </div>
 
                 </div>
@@ -35,8 +35,8 @@ const NavBar = () => {
                         <ul className='space-y-20' >
                             {navLinks.map((link, index) => (
                                 <>
-                                    <li key={index} spy={true} smooth={true} className="-rotate-90 text-lg items-center hover:text-red-600 " >
-                                        <a to={link.pathname}>{link.label}
+                                    <li key={index} spy={true} smooth={true} className="-rotate-90 " >
+                                        <a to={link.pathname} className="relative text-xl w-fit block after:block after:content-[''] after:absolute after:h-[3px] after:bg-red-700 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">{link.label}
                                         </a>
                                     </li>
 
@@ -64,17 +64,17 @@ const NavBar = () => {
 
                 {/* navbar for mobile */}
 
-                <div className={!isOpen ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-zinc-800 flex justify-center items-center'} >
-                    <div onClick={handelClick} className='fixed items-center  top-16 '>
-                        <IoClose  className="w-10 h-10 text-white cursor-pointer rounded-full border-b-gray-50 " />
+                <div className={!isOpen ? 'hidden' : 'absolute top-0 left-0 w-full h-screen backdrop-blur-sm flex justify-center items-center'} >
+                    <div onClick={handelClick} className='fixed right-[40px]  top-9 '>
+                        <IoClose  className="w-10 h-10 text-white cursor-pointer rounded-full bg-red-600 " />
                     </div>
-                    <div className='w-full justify-center items-center mx-auto'>
+                    <div className=' justify-center items-center mx-auto'>
                         <ul className='flex flex-col w-full space-y-10  justify-center'>
-                            {/* <XIcon className="w-6 h-6" /> */}
+                            
                             {navLinksMobile.map((link, index) => (
                                 <>
-                                    <li key={index} spy={true} smooth={true} className="mx-auto w-full text-center  space-x-6 text-3xl cursor-pointer  " >
-                                        <a to={link.pathname}>{link.label}
+                                    <li key={index} spy={true} smooth={true} className="  text-center  space-x-6 text-3xl cursor-pointer  " >
+                                        <a to={link.pathname}className=" after:block after:content-[''] after:h-[3px] after:bg-red-700 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center">{link.label}
                                         </a>
                                         {/* <div className=' border border-red-800 '></div> */}
                                     </li>
@@ -86,7 +86,7 @@ const NavBar = () => {
                     </div>
                     <div>
                         <ul className='flex flex-row absolute space-x-10 bottom-6 left-10 '>
-                            {/* <XIcon className="w-6 h-6" /> */}
+                            
                             {socialLinksMobile.map((link, index) => (
                                 <li key={index}  >
                                     <a href={link.url} target="_blank" rel="noopener noreferrer" className=' cursor-pointer '  >
